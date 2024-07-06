83c4b216c6c08a65af2060ee933ceb2b39fa0abc
Mikey Dowsett <mdowset1@stumail.jccc.edu>
2024-07-06 13:16:45 -0500

    Exponent, Factorial

---
+++ MiniTools/Views/Maths/FactorialView.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MiniTools.Views.Maths;
+
+public partial class FactorialView : ContentPage {
+    private FactorialViewModel vm;
+    
+    public FactorialView(FactorialViewModel vm) {
+        InitializeComponent();
+
+        BindingContext = vm;
+        this.vm = vm;
+    }
+}
--- MiniTools/obj/Debug/net8.0-ios/iossimulator-x64/MiniTools.AssemblyInfo.cs
+++ MiniTools/obj/Debug/net8.0-ios/iossimulator-x64/MiniTools.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MiniTools")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+b0f5488e137f31e48825ea53d42aa53ebe739ca5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+81fdfd01121ca8319d9d733fe4cf2b837915c711")]
 [assembly: System.Reflection.AssemblyProductAttribute("MiniTools")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MiniTools")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
